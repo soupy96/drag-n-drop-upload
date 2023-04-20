f94b433578499f32d6e8e763c9eeb94d16b3f581
Michael Campbell <michael.campbell14@gmail.com>
2023-04-20 14:35:31 -0400

    another commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const FileUploader = () => {
 
   useEffect(() => {
     uploadQueueF();
-  }, [uploadQueue, uploadQueueF]);
+  }, [uploadQueue]);
 
   const dropInFiles = (e) => {
     e.preventDefault();
